35ff039d0d2c294df2d470b7bf04d6f15d0d4083
u19gracues <u19gracies@cfgslive.com>
2024-10-10 17:43:43 +0100

    update jumping mechanic

--- Flappy bird/main.py
+++ Flappy bird/main.py
@@ -8,12 +8,16 @@ screenWidth = 1280
 screenHeight = 720
 speed = 0.25
 
+run = True
+jumping = False
+
 screen = pygame.display.set_mode(size=(screenWidth, screenHeight))
 pBody = pygame.Rect(50,50,50,50)
-p1 = MoveBird(5, 1, pBody, screen)
+p1 = MoveBird(5, 1, pBody, screen, jumping)
 
-run = True
+
 while run:
+    print(jumping)
     time.sleep(0.001)
 
     screen.fill((0,0,0))
@@ -21,16 +25,19 @@ while run:
     p1.gravityEffect()
     pygame.draw.rect(screen, (255,0,0), pBody)
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_SPACE] == True:
-        p1.birdUp()
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    key = pygame.key.get_pressed()
+    if key[pygame.K_SPACE] == True:
+        if not jumping:
+            p1.birdUp()
+            jumping = True
 
+    if not key[pygame.K_SPACE]:
+        jumping = False
+        
     pygame.display.update()
 
 pygame.quit()
--- Flappy bird/movingObjects.py
+++ Flappy bird/movingObjects.py
@@ -1,16 +1,16 @@
 import pygame
 
 class MoveBird:
-    def __init__(self, moveSpeed, gravity, player, screen):
+    def __init__(self, moveSpeed, gravity, player, screen, jumping):
         self.moveSpeed = moveSpeed
         self.gravity = gravity
         self.player = player
         self.screen = screen
+        self.jumping = jumping
 
     def gravityEffect(self):
         self.player.move_ip(0, self.gravity)
 
     def birdUp(self):
+        print('up!!')
         self.player.move_ip(0, -self.moveSpeed)
-
-##test
