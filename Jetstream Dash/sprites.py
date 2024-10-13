628715fd939bba91d6889da61b26123b1a95ca35
u19gracues <u19gracies@cfgslive.com>
2024-10-13 12:08:42 +0100

    bugfix

--- Jetstream Dash/sprites.py
+++ Jetstream Dash/sprites.py
@@ -12,6 +12,7 @@ class HandleSprite:
 
   def createSprite(self):
     sprite = pygame.Surface((self.width, self.height)).convert_alpha()
+    sprite.set_colorkey((0,0,0))
     sprite.blit(self.spritesheet, (0, 0), (0, self.frame*self.height, self.width, self.height))
     sprite = pygame.transform.scale(sprite, (self.width * self.scale, self.height * self.scale))
     return sprite
